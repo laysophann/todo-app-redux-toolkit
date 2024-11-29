387aa97a9e6b6215d55f951d0cb9be8769dfa17e
Lay Sophann <lay.sophann19@kit.edu.kh>
2024-11-29 14:47:33 +0700

    Create todo app with redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, ToggleComplete, deleteTodo } from "./feature/todo/todoSlicec";
 
-function App() {
+export default function App() {
+  const [text, setText] = useState("first");
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+
+  const handleInputChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (text) {
+      dispatch(addTodo(text));
+      setText("");
+    }
+  };
+
+  const handleToggleComplete = (id) => {
+    dispatch(ToggleComplete(id));
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch(deleteTodo(id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Todo App</h1>
+      <input type="text" value={text} onChange={handleInputChange} />
+      <button className="add" onClick={handleAddTodo}>
+        Add Todo
+      </button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id} className={todo.completed ? "completed" : ""}>
+            {todo.text}
+            <div>
+              <button
+                className="toggle"
+                onClick={() => handleToggleComplete(todo.id)}
+              >
+                {todo.completed ? "Mark Incomplete" : "Mark Complete"}
+              </button>
+              <button
+                className="delete"
+                onClick={() => handleDeleteTodo(todo.id)}
+              >
+                Delete
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
-
-export default App;
